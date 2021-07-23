ba09ea850752b6965392e872fe3efe2e77a3cb44
Athos-Mendes <atosneto@hotmail.com>
2021-07-23 13:34:43 -0300

    Learning about variables and constants with var, let and const.

---
+++ Section 4 - Fundamentals/2-VariablesConstants.js
@@ -0,0 +1,33 @@
+var valueA = 10
+let valueB = 100
+const valueC = 100
+
+console.log("> Value of the Variable valueA: ")
+console.log(valueA)
+
+console.log("> Value of the Variable valueB: ")
+console.log(valueB)
+
+console.log("> Value of the Constant valueA: ")
+console.log(valueC)
+
+var valueA = 100;
+valueB = 1000;
+
+/* The following lines will not work:
+
+let valueB = 1000; It's not possible to declare the same variables twice when let is used.
+const valueC = 10000; The same goes for constants. */
+
+console.log("")
+console.log("Changing some values:")
+console.log("")
+
+console.log("> Value of the Variable valueA: ")
+console.log(valueA)
+
+console.log("> Value of the Variable valueB: ")
+console.log(valueB)
+
+console.log("> Value of the Constant valueA: ")
+console.log(valueC)
