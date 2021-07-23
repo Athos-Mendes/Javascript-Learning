c446aaf0faf52aaf8d9d3c6626660bf7197dbe5e
Athos-Mendes <atosneto@hotmail.com>
2021-07-23 13:41:35 -0300

    Learning about Javascript typing.

---
+++ Section 4 - Fundamentals/3-TipagemFraca.js
@@ -0,0 +1,11 @@
+let variableA = 10
+
+console.log(typeof variableA) // It prints number
+
+variableA = 'Text'
+
+console.log(typeof variableA) // It prints string
+
+variableA = true
+
+console.log(typeof variableA) // It prints boolean
