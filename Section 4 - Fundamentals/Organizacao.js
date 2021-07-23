3d820b743fc5274b058f65d3d7e2932f25f7c0bd
Athos-Mendes <atosneto@hotmail.com>
2021-07-23 13:08:44 -0300

    Fixing the folder name, and studying ways of comments code.

---
+++ Section 4 - Fundamentals/Organizacao.js
@@ -0,0 +1,14 @@
+// It's possible to comment your code in one line only.
+
+console.log("This is a test.")
+console.log("Hello World!")
+
+/* You can also comment on multiple lines.
+It works here as well.*/
+
+/* 
+ *
+ *This is another way of doing the same thing.
+ * 
+ * 
+ */
